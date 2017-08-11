32e7dd2c02c5215746365f5c6f584c5b4720a3fc
Gabriel Deyo <gabedeyo@gmail.com>
2017-08-11 16:21:08 -0500

    Transition work

--- components/scripts/section-animate.js
+++ components/scripts/section-animate.js
@@ -8,21 +8,53 @@ var $game = $('.game');
 })();
 
 $('#intro-web-button').on('click', function() {
-        $middle.addClass('animated');
         $game.hide();
-        $web.show();        
-        $middle.addClass('slideOutRight');
-        $middle.bind('oanimationend animationend webkitAnimationEnd', function() {
-                $middle.css("display", "none");
+        $web.show();
+
+        if(!$web.hasClass('animated'))
+                $web.addClass('animated');
+        if(!$middle.hasClass('animated'))
+                $middle.addClass('animated');
+        
+        /* TRY LEFT OR RIGHT WHEN CONTENT IS IN PLACE */
+        if(!$web.hasClass('slideInLeft')){
+                $web.addClass('slideInLeft');
+        }
+        if(!$middle.hasClass('slideOutRight'))
+                $middle.addClass('slideOutRight');
+        
+        $web.one('animationend', function() {
+                //$middle.css("display", "none");
+                $middle.hide();
+                $web.removeClass('slideInLeft');
+                $middle.removeClass('slideOutRight');
         });
 });
 
+$('#go-right').on('click', function() {
+        $middle.show();
+
+        $web.addClass('slideOutLeft');
+        $middle.addClass('slideInRight');
+})
+
 $('#intro-game-button').on('click', function() {
-        $middle.addClass('animated');
         $web.hide();
         $game.show();
-        $middle.addClass('slideOutLeft');
-        $middle.bind('oanimationend animationend webkitAnimationEnd', function() {
+
+        if(!$game.hasClass('animated'))
+                $game.addClass('animated');
+        if(!$middle.hasClass('animated'))
+                $middle.addClass('animated');
+
+        if(!$game.hasClass('slideInRight'))
+                $game.addClass('slideInRight')
+        if(!$middle.hasClass('slideOutLeft'))
+                $middle.addClass('slideOutLeft')
+
+        $middle.one('animationend', function() {
                 $middle.css("display", "none");
+                $game.removeClass('slideInRight');
+                $middle.removeClass('slideOutLeft');
         });
 });
